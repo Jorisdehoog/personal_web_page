152ae52caf7c51b89fd1b16c31f68ae7c10c65f9
Joris <dehoogjoris@gmail.com>
2018-10-24 22:13:53 +0200

    made it work, now it doesnt anymore because i want to obfuscate my api details

--- js/getGitStuff.js
+++ js/getGitStuff.js
@@ -1,17 +1,39 @@
 // fetch the user
 
 
+// const secrets = fetch("githubToken.json")
+//     .then(response => response.json())
+//     .then(json => {
+//         console.log(json[0].client_id)
+//         const client_id = json[0].client_id;
+//         const client_secret = json[0].client_secret;
+//         return json;
+//     })
 
-const client_id = "Iv1.50fde0671b30c1ec";
-const client_secret = "dc958d06291f137ad4a909ac7a728ae41e68694a";
+async function loadJSON(url) {
+    const res = await fetch(url);
+    return await res.json();
+}
 
 user1 = "Jorisdehoog";
 
 const fetchUsers = async(user) =>{
+    // we need to get the credentials before getting the results.
+    console.log('we got this far')
+
+    secrets = loadJSON("githubToken.json").then(data => {
+        const client_id =  data[0].client_id;
+        const client_secret =  data[0].client_secret;
+        return [client_id, client_secret];
+    })
+
+    client_id = secrets[0];
+    client_secret = secrets[1];
+    
+    console.log(client_id)
+    
     const api_call = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
-
     const data = await api_call.json();
-
     return { data: data }
 };
 
@@ -19,8 +41,26 @@ const fetchUsers = async(user) =>{
 const showData = () => {
     fetchUsers(user1).then((res) => {
         console.log(res);
+        console.log(res.data.public_repos)
+        var lastUpdate = new Date(res.data.updated_at);
+        lastUpdate = lastUpdate.toJSON();
+        console.log(lastUpdate)
+        
+        document.getElementById("repoNumber").innerHTML = "Number of repos: " + res.data.public_repos;
+        var url = document.getElementById("blog");
+        url.href = res.data.blog;
+        url.innerHTML = res.data.blog;
+        document.getElementById("name").innerHTML = "Full name: " + res.data.name;
+        document.getElementById("update").innerHTML = "Last updated: " + res.data.updated_at;
+
+        // get and put profile pic
+        var pict = document.getElementById("profile");
+        pict.src = res.data.avatar_url;
     })
 };
 
 showData();
 
+{/* <p id="blog"></p>
+<p id="name"></p>
+<p id="update"></p> */}
